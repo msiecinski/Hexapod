1db83f61df62d92c954c14c865ca74ddf624d522
Michał <siecinski93@gmail.com>
2020-10-17 14:57:38 +0200

    Update pwm function
    Some changes in pwm genereting function.

--- hexapod_source_code/src/timefunc.cpp
+++ hexapod_source_code/src/timefunc.cpp
@@ -9,73 +9,108 @@ extern volatile uint32_t delayFlag;
 void PWM50Hz(void)
 {   
     static volatile int counter = 0;
-    static volatile int tmp = 100;
-    static volatile int tmp_angle = 100;
-    static volatile int tmpy = 100;
-    static volatile int tmp_angley = 100;
-
-	if(counter < (INTERRUPTCOUNTER))
+    if(counter < (INTERRUPTCOUNTER))
 		counter++;
-	else{
+	else
+    {
 		counter = 0;
         digitalWrite(LEG6_1, HIGH);
         digitalWrite(LEG6_2, HIGH);
+        digitalWrite(LEG6_3, HIGH);
+
         digitalWrite(LEG5_1, HIGH);
-        digitalWrite(LEG4_3, HIGH);
-        digitalWrite(LEG4_2, HIGH);
+        digitalWrite(LEG5_2, HIGH);
+        digitalWrite(LEG5_3, HIGH);
+
         digitalWrite(LEG4_1, HIGH);
-        digitalWrite(LEG3_3, HIGH);
+        digitalWrite(LEG4_2, HIGH);
+        digitalWrite(LEG4_3, HIGH);
+
+        digitalWrite(LEG3_1, HIGH);
         digitalWrite(LEG3_2, HIGH);
-	}
-    tmp_angle=angle_x;
-    if(tmp_angle<0)
-        tmp_angle*=-1;
-    tmp=(int)(100+tmp_angle*4.4);
-    if(tmp<100)
-        tmp=100;
-    if(tmp>500)
-        tmp=500;
-     tmp_angley=angle_y;
-    if(tmp_angley<0)
-        tmp_angley*=-1;
-    tmpy=(int)(100+tmp_angley*4.4);
-    if(tmpy<100)
-        tmpy=100;
-    if(tmpy>500)
-        tmpy=500;
+        digitalWrite(LEG3_3, HIGH);
+
+        digitalWrite(LEG2_1, HIGH);
+        digitalWrite(LEG2_2, HIGH);
+        digitalWrite(LEG2_3, HIGH);
+
+        digitalWrite(LEG1_1, HIGH);
+        digitalWrite(LEG1_2, HIGH);
+        digitalWrite(LEG1_3, HIGH);
+    }
     if(counter < (LIMITHI+1))   //skip all by~87.5% time(17.5ms)
     {   
-        if (counter ==  tmp) 
+        if (counter ==  hexapodControl[5].duty[0]) 
         {
             digitalWrite(LEG6_1, LOW);
         }
-        if (counter  ==  tmpy)
+        if (counter == hexapodControl[5].duty[1]) 
         {
             digitalWrite(LEG6_2, LOW);
         }
-        if (counter ==  500) 
+        if (counter == hexapodControl[5].duty[2]) 
+        {
+            digitalWrite(LEG6_3, LOW);
+        }
+        if (counter ==  hexapodControl[4].duty[0]) 
         {
             digitalWrite(LEG5_1, LOW);
         }
-        if (counter ==  350) 
+        if (counter ==  hexapodControl[4].duty[1]) 
         {
-            digitalWrite(LEG4_3, LOW);
+            digitalWrite(LEG5_2, LOW);
         }
-        if (counter ==  222) 
+        if (counter ==  hexapodControl[4].duty[2]) 
         {
-            digitalWrite(LEG4_2, LOW);
+            digitalWrite(LEG5_3, LOW);
         }
-        if (counter ==  hexapodControl[1].duty[0]) 
+        if (counter ==  hexapodControl[3].duty[0]) 
         {
             digitalWrite(LEG4_1, LOW);
         }
-        if (counter ==  hexapodControl[1].duty[1]) 
+        if (counter ==  hexapodControl[3].duty[1]) 
+        {
+            digitalWrite(LEG4_2, LOW);
+        }
+        if (counter ==  hexapodControl[3].duty[2]) 
+        {
+            digitalWrite(LEG4_3, LOW);
+        }
+        if (counter ==  hexapodControl[2].duty[0]) 
+        {
+            digitalWrite(LEG3_1, LOW);
+        }
+        if (counter ==  hexapodControl[2].duty[1]) 
+        {
+            digitalWrite(LEG3_2, LOW);
+        }
+        if (counter ==  hexapodControl[2].duty[2]) 
         {
             digitalWrite(LEG3_3, LOW);
         }
+        if (counter ==  hexapodControl[1].duty[0]) 
+        {
+            digitalWrite(LEG2_1, LOW);
+        }
+        if (counter ==  hexapodControl[1].duty[1]) 
+        {
+            digitalWrite(LEG2_2, LOW);
+        }
         if (counter ==  hexapodControl[1].duty[2]) 
         {
-            digitalWrite(LEG3_2, LOW);
+            digitalWrite(LEG2_3, LOW);
+        }
+        if (counter ==  hexapodControl[0].duty[0]) 
+        {
+            digitalWrite(LEG1_1, LOW);
+        }
+        if (counter ==  hexapodControl[0].duty[1]) 
+        {
+            digitalWrite(LEG1_2, LOW);
+        }
+        if (counter ==  hexapodControl[0].duty[2]) 
+        {
+            digitalWrite(LEG1_3, LOW);
         }
     }
 }
@@ -111,7 +146,7 @@ void DelayFunc()
         }
         else
         {
-        delayCounter++;
+            delayCounter++;
         }
     }
 }
