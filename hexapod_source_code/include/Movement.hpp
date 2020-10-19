815be01fe135f55ffd89ac3e49c643bea9525402
Michał <siecinski93@gmail.com>
2020-10-19 17:12:03 +0200

    Update header
    -added SetAllLegs function to the header

--- hexapod_source_code/include/Movement.hpp
+++ hexapod_source_code/include/Movement.hpp
@@ -6,6 +6,7 @@
 _Bool SetPosition(int, hexapod &);  //
 _Bool SetLeg(int,hexapod &);        
 void SetThreeLegs(int,hexapod *);
+void SetAllLegs(hexapod *);
 void SlideHorizontal(int ,int );    //horizontal movement of the entire robot
 void Move(movetype,int);            //direction,offset  
 
