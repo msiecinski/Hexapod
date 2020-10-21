a79bdf1f121fa140ea0aa17882b44893ae584104
Michał <siecinski93@gmail.com>
2020-10-21 20:54:12 +0200

    Added new files
    Fitrst implementation funtions used  for connect with dualshock 4 and used it to control robot movement.

---
+++ hexapod_source_code/include/Ps4control.hpp
@@ -0,0 +1,11 @@
+#ifndef Ps4control_H_
+#define Ps4control_H_
+
+#include <Global_defines.hpp>
+
+void SetupPS4(void);
+void LoopPS4(void);
+void ControlPS4(uint8_t, uint8_t);
+
+   
+#endif /* Ps4control_H_ */
